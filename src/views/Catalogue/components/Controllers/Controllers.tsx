45941e11d574681d0e7402d06d42f10d83b561bb
Seyhak <seyhakly@gmail.com>
2023-11-13 21:31:05 +0100

    TODO update, search reset and info

--- src/views/Catalogue/components/Controllers/Controllers.tsx
+++ src/views/Catalogue/components/Controllers/Controllers.tsx
@@ -1,9 +1,18 @@
-import { Box, ButtonGroup, IconButton } from "@mui/material"
+import {
+  Box,
+  ButtonGroup,
+  IconButton,
+  Typography,
+  Tooltip,
+} from "@mui/material"
 import { ConfirmDeleteButtonWithDialog } from "../ConfirmDeleteButtonWithDialog/ConfirmDeleteButtonWithDialog"
 
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline"
 import ModeEditIcon from "@mui/icons-material/ModeEdit"
 import SaveIcon from "@mui/icons-material/Save"
+import RestartAltIcon from "@mui/icons-material/RestartAlt"
+import { useControllers } from "./useControllers"
+import "./Controllers.sass"
 
 export type ControllersProps = {
   isItemSelected: boolean
@@ -21,15 +30,35 @@ export const Controllers = ({
   onEditClick,
   onDeleteClick,
 }: ControllersProps) => {
+  const { search, onResetSearchClick } = useControllers()
+
   return (
     <Box className="controllers-wrapper">
+      <Box className="search-status-wrapper">
+        {!!search && (
+          <Typography variant="h6">Results for "{search}"</Typography>
+        )}
+      </Box>
       <ButtonGroup
         variant="outlined"
         aria-label="outlined primary button group"
       >
-        <IconButton aria-label="add" color="primary" onClick={onAddClick}>
-          <AddCircleOutlineIcon />
-        </IconButton>
+        {!!search && (
+          <Tooltip title="Reset search">
+            <IconButton
+              aria-label="reset-search"
+              color="primary"
+              onClick={onResetSearchClick}
+            >
+              <RestartAltIcon />
+            </IconButton>
+          </Tooltip>
+        )}
+        <Tooltip title="Add position">
+          <IconButton aria-label="add" color="primary" onClick={onAddClick}>
+            <AddCircleOutlineIcon />
+          </IconButton>
+        </Tooltip>
         {isItemSelected && (
           <>
             {isEditModeOn && (
@@ -41,9 +70,15 @@ export const Controllers = ({
                 <SaveIcon />
               </IconButton>
             )}
-            <IconButton onClick={onEditClick} aria-label="edit" color="primary">
-              <ModeEditIcon />
-            </IconButton>
+            <Tooltip title="Edit position">
+              <IconButton
+                onClick={onEditClick}
+                aria-label="edit"
+                color="primary"
+              >
+                <ModeEditIcon />
+              </IconButton>
+            </Tooltip>
             <ConfirmDeleteButtonWithDialog onDeleteClick={onDeleteClick} />
           </>
         )}
